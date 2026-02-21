5eb74f20ab3767c6bc01cc4d115b90fed37807ac
MavenW <ytsiuioh45@gmail.com>
2026-02-20 22:35:08 -0500

    Compilation error Message

---
+++ drivers/video/vga/vga_mouse.c
@@ -0,0 +1,1 @@
+#error "vga_mouse.c merged driver removed; use drivers/input/ps2mouse.c and drivers/video/vga/vga_cursor.c instead"
---
+++ drivers/video/vga/vga_mouse.h
@@ -0,0 +1,2 @@
+#error "vga_mouse.h has been split: use <input/ps2mouse.h> for PS/2 state and vga_cursor.h for drawing"
+
